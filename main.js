3875c03d195042a2e6d46c44fc03505a77f9bcd7
luoxin <goodmanluoxin@163.com>
2018-01-06 11:42:48 +0800

    完善了功能

--- main.js
+++ main.js
@@ -7,12 +7,60 @@ var pageHeight = document.documentElement.clientHeight;
 var linetWidth = 10; //这是点的直径，连接点与点的直线应该是直径的两倍
 var earserOn = false;
 var Operating = false; //
+var paintColor = 'black';
 var startPoint;
 
-//drawLine(100, 100, 0, 0);
 
 reSize();
 
+
+/*
+function start(canvas){
+    //特性检测
+    if('ontouchstart' in document.body){
+
+    }else{
+
+    }
+}
+*/
+
+//////////////////////////////手机端
+
+canvas.ontouchstart = function(touch){
+    if(earserOn){
+        createEarser(touch.touches[0].clientX, touch.touches[0].clientY, linetWidth, linetWidth);
+        Operating = true;
+    }else{
+        drawCir(touch.touches[0].clientX, touch.touches[0].clientY, linetWidth);
+        startPoint = {'x': touch.touches[0].clientX, 'y': touch.touches[0].clientY};
+        Operating = true;
+    }
+}
+
+canvas.ontouchmove = function(touch){
+    if(earserOn){
+        if(Operating){
+            createEarser(touch.touches[0].clientX, touch.touches[0].clientY, linetWidth, linetWidth);
+        }
+
+    }else{
+
+        if(Operating){
+            drawCir(touch.touches[0].clientX, touch.touches[0].clientY, linetWidth);
+            var endPoint = {'x': touch.touches[0].clientX, 'y': touch.touches[0].clientY};
+            drawLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+
+        }
+        startPoint = endPoint;
+    }
+}
+
+canvas.ontouchend = function(){
+    Operating = false;
+}
+
+//////////////////////////////PC端
 canvas.onmousedown = function(mouse){
     if(earserOn){
         createEarser(mouse.clientX, mouse.clientY, linetWidth, linetWidth);
@@ -51,6 +99,7 @@ canvas.onmouseup = function(mouse){
 function drawCir(x, y, radius){
     context.beginPath();
     context.arc(x, y, radius, 0, Math.PI*2);
+    context.fillStyle = paintColor;
     context.fill();
 
 }
@@ -60,6 +109,7 @@ function drawLine(startX, startY, endX, endY){
     context.moveTo(startX, startY);
     context.lineTo(endX, endY);
     context.lineWidth = linetWidth*2;
+    context.strokeStyle = paintColor;
     context.stroke();
 }
 
@@ -75,14 +125,79 @@ function createEarser(x, y, width, height){
 //橡皮擦点击
 earser.onclick = function(){
     earserOn = true;
-    action.className = 'action x';
+    earser.classList.add('active');
+    penscil.classList.remove('active');
 }
 
 //画笔点击
 penscil.onclick = function(){
     earserOn = false;
-    action.className = 'action';
+    penscil.classList.add('active');
+    earser.classList.remove('active');
 }
 
+earser.onclick = function(){
+    earserOn = true;
+    earser.classList.add('active');
+    penscil.classList.remove('active');
+}
+
+black.onclick = function(){
+    black.classList.add('active');
+    red.classList.remove('active');
+    blue.classList.remove('active');
+    green.classList.remove('active');
+    paintColor = 'black';
+
+}
+
+red.onclick = function(){
+    red.classList.add('active');
+    blue.classList.remove('active');
+    green.classList.remove('active');
+    black.classList.remove('active');
+    paintColor = 'red';
+
+}
+
+green.onclick = function(){
+    green.classList.add('active');
+    blue.classList.remove('active');
+    red.classList.remove('active');
+    black.classList.remove('active');
+    paintColor = 'green';
+}
+
+blue.onclick = function(){
+    blue.classList.add('active');
+    red.classList.remove('active');
+    green.classList.remove('active');
+    black.classList.remove('active');
+    paintColor = 'blue';
+}
+
+clear.onclick = function(){
+    context.clearRect(0, 0, pageWidth, pageHeight);
+}
+
+down.onclick = function(){
+    var a = document.createElement('a');
+    document.body.appendChild(a);
+    console.log('a');
+    var url = canvas.toDataURL('image/png');
+    a.href = url;
+    a.download = 'xxx';
+    a.click();
+}
+
+small.onclick = function(){
+
+    linetWidth = 4;
+}
+
+big.onclick = function(){
+
+    linetWidth = 10;
+}
 
 
